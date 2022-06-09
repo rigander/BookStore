05afc2ee1befa37ff7f8b231f4c7c5cb1df7cfc5
Andrey Komov <andrewkomov@gmail.com>
2022-06-09 22:08:01 +0300

    added files

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+const {src, dest, watch, series} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+
+// Sass task
+function scssTask(){
+    return src('scss/product-page-styles.scss', { sourcemaps: true })
+        .pipe(sass())
+        .pipe(dest('css', { sourcemaps: '.'}));
+}
+// Browsersync tasks
+function browsersyncServer(cb){
+    browsersync.init({
+        server: {
+            baseDir: '.'
+        }
+    })
+    cb();
+}
+function browsersyncReload(cb){
+    browsersync.reload();
+    cb();
+}
+//Watch task
+function watchTask(){
+    watch('*.html', browsersyncReload);
+    watch(['scss/**/*.scss'], series(scssTask, browsersyncReload));
+}
+
+//default gulp task
+
+exports.default = series(
+    scssTask,
+    browsersyncServer,
+    watchTask
+)
+
